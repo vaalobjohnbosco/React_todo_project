b856de6f0d0d1d8f60306bc5adc855f90db488bf
vaalob <vaalobjohnbosco@gmail.com>
2021-10-23 23:42:14 -1200

    My react todo project

---
+++ src/components/TodoInput.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        const {item, handleChange, handleSubmit,editItem } = this.props
+        return (
+            <div className="card card-body mt-3">
+              <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                           <div className="input-group-text bg-primary text-white">
+                                <i  className="fas fa-book"/>
+                           </div>
+                                <input  type="text" 
+                                        className="form-control text-capitalize" 
+                                        placeholder="enter todo items" 
+                                        value = {item}
+                                        onChange = {handleChange}
+                                />
+                    </div>
+                    <div className="list-group">
+                           <button type="submit" className= {editItem ? "btn btn-sm btn-block btn-success mt-3" :"btn btn-sm btn-block btn-primary mt-3"}>
+                               {editItem ? "Edit Item" : "Add Item"} 
+                           </button>
+                    </div>
+              </form>
+            </div>
+        )
+    }
+}
